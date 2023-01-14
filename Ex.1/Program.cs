c8381b9371e447f7249d2eae56d20a8baa922101
NikolaiB <altnikolos95@gmail.com>
2023-01-15 00:20:13 +0300

    Add project files.

---
+++ Ex.1/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Ex._1
+{
+
+
+
+    internal class Program
+    {
+        internal class WrongLoginException : Exception
+        {
+            public WrongLoginException() : base("Wrong login!") { }
+            public WrongLoginException(string message) : base(message) { }
+        }
+
+        internal class WrongPasswordExeception : Exception
+        {
+            public WrongPasswordExeception() : base("Wrong password") { }
+
+            public WrongPasswordExeception(string message) : base(message) { }
+        }
+
+
+        internal class Authorization
+        {
+            private string _login;
+            private string _password;
+
+            public Authorization(string login, string password, string confirmPassword)
+            {
+                if (AuthorizationChecking(login, password, confirmPassword))
+                {
+                    _login = login;
+                    _password = password;
+                }
+            }
+
+            public static bool AuthorizationChecking(string login, string password, string confirmPassword)
+            {   
+                if (login.Length > 20 || login.Any(s => s.Equals(" ")))
+                {
+                    throw new WrongLoginException("Password should be without any spaces " +
+                        "and should contains only 20 simbols or less");
+                }
+                if (password != confirmPassword)
+                {
+                    throw new WrongPasswordException("Wrong password!");
+                }
+
+                return true;
+            }
+
+
+
+        }
+
+        static void Main(string[] args)
+
+        {   
+            try
+            { 
+                Authorization aut = new Authorization("VicMur", "2122", "2121");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message); 
+            }
+           
+        }
+
+    }
+}
---
+++ Ex.1/WrongPasswordException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace Ex._1
+{
+    [Serializable]
+    internal class WrongPasswordException : Exception
+    {
+        public WrongPasswordException()
+        {
+        }
+
+        public WrongPasswordException(string? message) : base(message)
+        {
+        }
+
+        public WrongPasswordException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected WrongPasswordException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
